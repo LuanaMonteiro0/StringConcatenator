9e8591e145ef7123ac0e83ef4f0773a4f4e10105
Luana Monteiro <luanagiovanamonteiro@gmail.com>
2026-09-14 20:45:47 -0300

    HomeScreen Logic implemented

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3029972/stringconcatenator/ui/composable/HomeScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3029972/stringconcatenator/ui/composable/HomeScreen.kt
@@ -8,22 +8,34 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.prdm.sc3029972.stringconcatenator.ui.theme.StringConcatenatorTheme
 
 @Composable
-fun HomeScreen(modifier: Modifier) {
+fun HomeScreen(modifier: Modifier, textToShow: String = "", textToConcatenate: String = "", onAddWordClick: (String) -> Unit) {
+
+    var textToShow by remember { mutableStateOf(value = textToShow) }
+
+    if(textToConcatenate != ""){
+        textToShow = "$textToShow $textToConcatenate"
+    }
 
     Column(modifier = modifier.fillMaxSize()){
         Text(
             modifier = Modifier.fillMaxWidth(),
-            text = ""
+            text = textToShow
         )
 
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = {
+                onAddWordClick(textToShow)
+            }
         ) {
             Text(
             modifier = Modifier.fillMaxWidth(),
@@ -33,7 +45,9 @@ fun HomeScreen(modifier: Modifier) {
 
         Button(
             modifier = Modifier.fillMaxWidth(),
-            onClick = {}
+            onClick = {
+                textToShow = ""
+            }
         ) {
             Text(
                 modifier = Modifier.fillMaxWidth(),
@@ -59,7 +73,12 @@ fun HomeScreen(modifier: Modifier) {
 fun HomeScreenPreview(){
     StringConcatenatorTheme() {
         Surface{
-            HomeScreen(modifier = Modifier)
+            HomeScreen(
+                modifier = Modifier,
+                textToShow = "",
+                textToConcatenate = "",
+                onAddWordClick = {}
+            )
         }
     }
 }
